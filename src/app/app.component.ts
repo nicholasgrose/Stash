eba5ac158559843c494e624b1f70654d0789acee
Arkinion <nicholasgr44@gmail.com>
2018-10-20 10:34:09 -0400

    Made the sidebar docked on the side and open on mouseover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sidebarToggled = true;
+  sidebarToggled = false;
 
   toggleSidebar() {
     this.sidebarToggled = !this.sidebarToggled;
