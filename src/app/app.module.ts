92d98edebb9a3e3a701bd714b91a2ef2bc1cf3c5
Arkinion <nicholasgr44@gmail.com>
2018-10-19 23:52:17 -0400

    Added a sidebar component and fixed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
